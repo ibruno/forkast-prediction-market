5a55e1dbec7379553447ca2ed302af8e70e84a53
Bruno Tomé <ibrunotome@gmail.com>
2025-08-08 12:47:27 -0300

    fix: provide base url for build time

--- src/components/layout/NavigationTabs.tsx
+++ src/components/layout/NavigationTabs.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import NavigationTab from '@/components/layout/NavigationTab'
 import { getMainCategories } from '@/lib/mockData'
 
@@ -7,13 +8,15 @@ export default async function NavigationTabs() {
   return (
     <nav className="sticky top-14 z-10 border-b bg-background">
       <div className="container flex gap-6 overflow-x-auto py-1 text-sm font-medium">
-        {categories.map((category, index) => (
-          <div key={category.id} className="flex items-center">
-            <NavigationTab category={category} />
+        <Suspense>
+          {categories.map((category, index) => (
+            <div key={category.id} className="flex items-center">
+              <NavigationTab category={category} />
 
-            {index === 1 && <div className="mr-0 ml-6 h-4 w-px bg-border" />}
-          </div>
-        ))}
+              {index === 1 && <div className="mr-0 ml-6 h-4 w-px bg-border" />}
+            </div>
+          ))}
+        </Suspense>
       </div>
     </nav>
   )
