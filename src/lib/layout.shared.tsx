fad60f76062a8d1f41e199e6be790912cdecc088
Bruno Tomé <ibrunotome@gmail.com>
2025-09-26 21:41:52 -0300

    docs: add logo

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,10 +1,24 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
 import { ExternalLinkIcon } from 'lucide-react'
+import { sanitizeSvg } from '@/lib/utils'
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: `${process.env.NEXT_PUBLIC_SITE_NAME} Docs`,
+      title: (
+        <>
+          <div
+            className="size-6"
+            dangerouslySetInnerHTML={{
+              __html: sanitizeSvg(process.env.NEXT_PUBLIC_SITE_LOGO_SVG!),
+            }}
+          />
+          <span className="font-medium [.uwu_&]:hidden [header_&]:text-[15px]">
+            {`${process.env.NEXT_PUBLIC_SITE_NAME} Docs`}
+          </span>
+        </>
+      ),
+      transparentMode: 'top',
     },
     githubUrl: 'https://github.com/forkast-prediction-market/forkast-prediction-market',
     links: [
