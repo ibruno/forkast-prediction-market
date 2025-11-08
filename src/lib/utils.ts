ef6aee6f7a5090fb1c4c58131b2cc406a9e39967
Bruno Tomé <ibrunotome@gmail.com>
2025-11-08 16:01:04 -0300

    chore: apply no-redos-regex

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,13 +21,38 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+const SCRIPT_OPEN_TAG = '<script'
+const SCRIPT_CLOSE_TAG = '</script>'
+
+function stripScriptTags(svg: string) {
+  const lower = svg.toLowerCase()
+  let cursor = 0
+  let sanitized = ''
+
+  while (cursor < svg.length) {
+    const start = lower.indexOf(SCRIPT_OPEN_TAG, cursor)
+    if (start === -1) {
+      sanitized += svg.slice(cursor)
+      break
+    }
+
+    sanitized += svg.slice(cursor, start)
+    const end = lower.indexOf(SCRIPT_CLOSE_TAG, start)
+    if (end === -1) {
+      break
+    }
+
+    cursor = end + SCRIPT_CLOSE_TAG.length
+  }
+
+  return sanitized
+}
+
 export function sanitizeSvg(svg: string) {
-  return svg
-    .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
-    .replace(/on\w+="[^"]*"/g, '')
-    .replace(/on\w+='[^']*'/g, '')
+  return stripScriptTags(svg)
+    .replace(/on\w+=["'][^"']*["']/gi, '')
     .replace(/javascript:/gi, '')
-    .replace(/data:/gi, '')
+    .replace(/data:(?!image\/)/gi, '')
 }
 
 export function triggerConfetti(color: 'primary' | 'yes' | 'no', event?: any) {
